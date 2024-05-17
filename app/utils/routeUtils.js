4565626c7b89e86ab046b4933aa840a5a4823a63
damanjotDev <damanjot@devronins.com>
2024-05-17 23:35:37 +0530

    downgrade swagger version

--- app/utils/routeUtils.js
+++ app/utils/routeUtils.js
@@ -141,7 +141,7 @@ let createSwaggerUIForRoutes = (app, routes = []) => {
 	// routes.forEach(route => {
 	// 	swJson.swaggerDoc.addNewRoute(route.joiSchemaForSwagger, route.path, route.method.toLowerCase(), route.auth);
 	// });
-	const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+	const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.0/swagger-ui.min.css";
 	const swaggerDocument = require('../../swagger.json');
 	let swagger_credential = {}
 	swagger_credential[`${process.env.DOCUMENTATION_USER}`] = process.env.DOCUMENTATION_PASSWORD
